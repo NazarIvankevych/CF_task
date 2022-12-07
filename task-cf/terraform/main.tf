3ce5e4f2053a0e58d27ba72f754c93994a0ac04f
NazarIvankevych <nazar.ivankevych@gmail.com>
2022-12-07 12:41:51 +0200

    Add new resources, bigquery, tables

--- task-cf/terraform/main.tf
+++ task-cf/terraform/main.tf
@@ -10,34 +10,90 @@ terraform {
       source = "hashicorp/google"
       version = "4.44.1"
     }
+  # }
+  # backend "gcp" {
+  #   prefix = "/task-cf"
+  # }
   }
 }
 
 provider "google" {
   # Configuration options
-  project = "task-cf-370710"
-  region = "us-central1"
-  zone = "us-central1-c"
+  project = var.project_id
+  region = var.region
+  zone = var.zone
 }
 
-resource "google_storage_bucket" "func_cf" {
-  name = "func_cf_tf"
-  location = "US"
+resource "google_storage_bucket" "task-cf-bucket" {
+  name = "${var.project_id}-bucket"
+  location = var.region
+  force_destroy = true
 }
 
-resource "google_storage_bucket_object" "function_code_archive" {
-  name = "function"
-  source = "function.zip"
-  bucket = google_storage_bucket.func_cf.name
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir = "./function"
+  output_path = "/tmp/function.zip"
 }
 
-resource "google_cloudfunctions_function" "func_cf_tf" {
-  name = "func_cf_tf"
-  source_archive_bucket = google_storage_bucket.func_cf.name
-  source_archive_object = google_storage_bucket_object.function_code_archive.name
-  available_memory_mb = 128
-  entry_point         = "main"
+resource "google_storage_bucket_object" "zip" {
+  source = data.archive_file.source.output_path
+  content_type = "application/zip"
+  name = "src-${data.archive_file.source.output_md5}.zip"
+  bucket = google_storage_bucket.task-cf-bucket.name
+
+  depends_on = [
+    google_storage_bucket.task-cf-bucket,
+    data.archive_file.source
+  ]
+}
+
+resource "google_cloudfunctions_function" "task-cf-function" {
+  name = "task-cf-function"
   runtime             = "python38"
+
+  source_archive_bucket = google_storage_bucket.task-cf-bucket.name
+  source_archive_object = google_storage_bucket_object.zip.name
+  
+  entry_point         = "main"
   trigger_http = true
-  timeout = 540
+  
+  available_memory_mb = 128
+  timeout = 60
+
+  depends_on = [
+    google_bigquery_dataset.task-cf-dataset,
+    google_storage_bucket.task-cf-bucket,
+    google_storage_bucket_object.zip
+  ]
 }
+
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.task-cf-function.project
+  region         = google_cloudfunctions_function.task-cf-function.region
+  cloud_function = google_cloudfunctions_function.task-cf-function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+
+  depends_on = [
+    google_cloudfunctions_function.task-cf-function
+  ]
+}
+
+resource "google_bigquery_dataset" "task-cf-dataset" {
+  dataset_id  = var.dataset_id
+  description = "This dataset is public"
+  location    = "US"
+}
+
+resource "google_bigquery_table" "task-cf-table" {
+  dataset_id = var.dataset_id
+  table_id   = var.table_id
+  schema     = file("/task-cf/schemas/bq_table_schema/task-cf-raw.json")
+
+  depends_on = [
+    google_bigquery_dataset.task-cf-dataset
+  ]
+}
+
