f5fbe89369bf2c7d296c431b99a4557b4052f4c2
NazarIvankevych <nazar.ivankevych@gmail.com>
2023-01-19 21:25:19 +0200

    Rewrite sql

--- task-af/sql/af_task_query.sql
+++ task-af/sql/af_task_query.sql
@@ -1,12 +1,13 @@
 WITH
 multiple AS (
     SELECT
-        output_table.age*output_table.number,
-        output_table.timestamp
+        *,
+        age*number,
+        timestamp
     FROM
-        `{{ params.AF_TASK_INPUT_TABLE }}` output_table
+        `{{ params.AF_TASK_INPUT_TABLE }}` 
     WHERE
         output_table.timestamp > DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL -1 HOUR)
 )
 SELECT * FROM multiple
--- SELECT *, number*age as multiple FROM `{{ params.AF_TASK_INPUT_TABLE }}`
+-- SELECT *, number*age AS multiple FROM `{{ params.AF_TASK_INPUT_TABLE }}` 
